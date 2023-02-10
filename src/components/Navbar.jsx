76ce7a176056d598155cd6b8f9a65e83176891e4
Ayomide11705 <99465847+Ayomide11705@users.noreply.github.com>
2023-02-10 01:33:16 +0100

    third commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <header >
-        
+    <header className='header'>
+      <div className='logo'>
+        <h1>
+          <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48"><g fill="none" fill-rule="evenodd"><circle cx="24" cy="24" r="24" fill="#FFF" /><path fill="#0B0D17" d="M24 0c0 16-8 24-24 24 15.718.114 23.718 8.114 24 24 0-16 8-24 24-24-16 0-24-8-24-24z" /></g></svg>
+        </h1>
+      </div>
+      <nav>
+        <ul>
+          <li><h1>00</h1><p>HOME</p></li>
+          <li><h1>01</h1><p>DESTINATION</p></li>
+          <li><h1>02</h1><p>CREW</p></li>
+          <li><h1>03</h1><p>TECHNOLOGY</p></li>
+        </ul>
+      </nav>
     </header>
   )
 }
